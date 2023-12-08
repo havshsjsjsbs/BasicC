6bf8bbc77bcb10501589b2850e1a607e62364201
Shi Shasha <shishashalisa@163.com>
2023-12-08 16:16:01 +0800

    [新增]新增数组的非法访问 和静态数组的优劣势

--- demoArray/demoArray.c
+++ demoArray/demoArray.c
@@ -11,15 +11,15 @@ int main ()
     int a;
     printf("a:%d\n", a);
     /* 方法一： 使用定义即初始化 */
-    #if 0
+    #if 1
     int array[BUFFER_SIZE] = { 0 };
     #else
     int array[BUFFER_SIZE];
     #endif
 
     /* 清除脏数据 */
-    memset (array, 0 , sizeof(array));
-    printf("=============\n");
+    // memset (array, 0 , sizeof(array));
+    //printf("=============\n");
     for(int idx = 0; idx < BUFFER_SIZE; idx++)
     {
         printf("%d, %d\n", idx, array[idx]);
@@ -38,6 +38,10 @@ int main ()
     /* 0X0F + 0X01 = 0X10 */
     #endif
     
+    /* 数组的越界访问 */
+    /* 判断索引的有效性 */
+    printf("array[-1]:%d\n",  array[-1]);
+    printf("array[10]:%d\n",  array[10]);
 
     return 0;
 }
